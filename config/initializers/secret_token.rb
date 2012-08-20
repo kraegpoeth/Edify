d42e930d9f0538db57588442c87803c2e1d0ef99
Kraegpoeth <Kraegpoeth@gmail.com>
2012-08-20 13:56:24 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Edify::Application.config.secret_token = '6715bcaebab79120adf8132a153419a518a5aaff1ef1ea8050e52dacc0ef37e43e64b1eb3f4b4c38f3b92f40bd8797c377910f648906928efb48ad2622d355cb'
